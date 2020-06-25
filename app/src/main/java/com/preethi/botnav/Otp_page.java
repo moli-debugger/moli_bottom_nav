27639227ef532a63c16441d435e6d28e58e5e7eb
moli-debugger <60547028+moli-debugger@users.noreply.github.com>
2020-06-25 17:55:22 +0530

    2nd commit

---
+++ app/src/main/java/com/preethi/botnav/Otp_page.java
@@ -0,0 +1,14 @@
+package com.preethi.botnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Otp_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_page);
+    }
+}
